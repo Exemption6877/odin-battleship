d8602a8b112f0e3fbb38433fa98b364189d5f4ae
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-05 04:55:06 +0300

    Ensure jest is working

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
-import path from "path";
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import { fileURLToPath } from "url";
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
-export default {
+module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
