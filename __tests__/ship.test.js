3cc35aba7cb36ad67c3af66faccd791be667dec4
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-05 19:29:03 +0300

    Reorganize tests

---
+++ __tests__/ship.test.js
@@ -0,0 +1,34 @@
+import { Carrier } from "../src";
+describe("Ship Class Testing", () => {
+  let newShip;
+
+  beforeEach(() => {
+    newShip = new Carrier();
+  });
+
+  test("Carrier hit reaction", () => {
+    newShip.hit();
+    expect(newShip.currentHits).toBe(1);
+  });
+
+  test("Carrier 3 hits", () => {
+    newShip.hit();
+    newShip.hit();
+    newShip.hit();
+    expect(newShip.currentHits).toBe(3);
+  });
+
+  test("Carrier sink reaction", () => {
+    for (let i = 0; i < newShip.length; i++) {
+      newShip.hit();
+    }
+    expect(newShip.isSunk()).toBeTruthy();
+  });
+
+  test("Carrier not sink reaction", () => {
+    for (let i = 0; i < newShip.length - 1; i++) {
+      newShip.hit();
+    }
+    expect(newShip.isSunk()).toBeFalsy();
+  });
+});
--- src/index.js
+++ src/index.js
@@ -70,6 +70,7 @@ class Gameboard {
 1  [~] [~] [~] [~] [~] [~] [~] [~] [~] [~]
 0  [~] [~] [~] [~] [~] [~] [~] [~] [~] [~]
     0   1   2   3   4   5   6   7   8   9
+    
 Carrier
 Battleship
 Destroyer
