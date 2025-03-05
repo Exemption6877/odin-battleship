2c950cfd317748db34532a5fbb27fa6de2ab2a0c
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-05 05:16:43 +0300

    Add Ship class, other ships subclasses

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-export default function sum(a, b) {
-  return a + b;
+class Ship {
+  constructor(maxHits) {
+    this.maxHits = maxHits;
+    this.currentHits = 0;
+  }
+
+  hit() {}
+  isSunk() {}
+}
+
+class Carrier extends Ship {
+  constructor() {
+    super(5);
+  }
+}
+
+class Battleship extends Ship {
+  constructor() {
+    super(4);
+  }
+}
+
+class Destroyer extends Ship {
+  constructor() {
+    super(3);
+  }
+}
+
+class Submarine extends Ship {
+  constructor() {
+    super(3);
+  }
+}
+
+class PatrolBoat extends Ship {
+  constructor() {
+    super(2);
+  }
 }
