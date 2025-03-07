53657c66d9b9eb956ff96245b37081583e6514aa
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-07 04:00:53 +0300

    Test the bot's logic

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -1,4 +1,4 @@
-import { Player } from "../src/player";
+import { Player, PlayerBot } from "../src/player";
 import { Gameboard } from "../src/gameboard";
 
 describe("Player gameboard tests", () => {
@@ -9,5 +9,28 @@ describe("Player gameboard tests", () => {
 
   test("Player has gameboard", () => {
     expect(player1.personalGameboard).toBeInstanceOf(Gameboard);
+  });
+
+  describe("PlayerBot tests", () => {
+    test("PlayerBot makes random move and returns it", () => {
+      const bot = new PlayerBot();
+      expect(bot.makeMove()).toBeDefined();
+    });
+  });
+
+  describe("PlayerBot tests", () => {
+    test("PlayerBot strikes adjusted cell", () => {
+      const bot = new PlayerBot();
+      const mockRandom = jest.fn();
+      mockRandom.mockReturnValueOnce(3).mockReturnValueOnce(5);
+
+      const originalRandomGen = bot.__randomGen;
+      bot.__randomGen = mockRandom;
+
+      const result = bot.makeMove(true, [2, 5]);
+      expect(result).toEqual([2, 6]);
+
+      bot.__randomGen = originalRandomGen;
+    });
   });
 });
