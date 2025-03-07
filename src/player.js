b089848c0745cf67c5c18df2ff8354381ebf7797
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-07 04:00:11 +0300

    Add bot's logic to attack adjustened cells on hit

--- src/player.js
+++ src/player.js
@@ -10,8 +10,35 @@ class PlayerBot extends Player {
   constructor() {
     super();
   }
+  __randomGen(num) {
+    return Math.floor(Math.random() * num);
+  }
   // on hit, hit adjustened cell next
-  makeMove() {}
+  // to make the AI even better, I could notify it on isSunk() to not hit obvious dead cells.
+  makeMove(prevHit = false, prevCoord = null) {
+    if (prevHit) {
+      const [prevX, prevY] = prevCoord;
+      // randomize to choose whether to hit x or y
+      if (this.__randomGen(2) === 0) {
+        // choose -1 or +1
+        if (this.__randomGen(2) === 0) {
+          return [prevX - 1, prevY];
+        } else {
+          return [prevX + 1, prevY];
+        }
+      } else {
+        if (this.__randomGen(2) === 0) {
+          return [prevX, prevY - 1];
+        } else {
+          return [prevX, prevY + 1];
+        }
+      }
+    } else {
+      let x = this.__randomGen(10);
+      let y = this.__randomGen(10);
+      return [x, y];
+    }
+  }
 }
 
 export { Player, PlayerBot };
