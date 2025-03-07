7b60b8b2ec97e4fca287e0b6bfe2994d391f7fd0
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-07 04:12:17 +0300

    Fix imports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-import { Gameboard } from "./gameboard";
-import { Player, PlayerBot } from "./player";
-import { Ship } from "./ship";
-import { Carrier, Battleship, Destroyer, Submarine, PatrolBoat } from "./ship";
+import { Gameboard } from "./gameboard.js";
+import { Player, PlayerBot } from "./player.js";
+import { Ship } from "./ship.js";
+import {
+  Carrier,
+  Battleship,
+  Destroyer,
+  Submarine,
+  PatrolBoat,
+} from "./ship.js";
