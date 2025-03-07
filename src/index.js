553a9d7c7100aa9942bd36f9d6bc089a74f1fd01
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-03-08 00:57:30 +0300

    Hide player choice on click

--- src/index.js
+++ src/index.js
@@ -18,14 +18,21 @@ function generateButton() {
     button.classList.add("btn-choice");
     button.innerText = `Play against ${player}`;
     button.value = player;
+    //event listener here?
 
+    button.addEventListener("click", () => {
+      const wrapper = document.querySelector(".choose-opponent");
+      wrapper.classList.add("hidden");
+    });
     return button;
   };
 
   const chooseOpponent = (player1, player2) => {
     const wrapper = document.createElement("div");
     wrapper.classList.add("choose-opponent");
-    wrapper.append(playAgainst(player1), playAgainst(player2));
+    const button1 = playAgainst(player1);
+    const button2 = playAgainst(player2);
+    wrapper.append(button1, button2);
 
     return wrapper;
   };
@@ -33,13 +40,26 @@ function generateButton() {
   return { chooseOpponent };
 }
 
+// function gameplay() {
+//   // setup - show a single board, let the player to place ships,
+//   // press confirm to push changes.
+
+//   const setup = () => {};
+// }
+
 const gameStartButton = document.querySelector("#game-start");
 gameStartButton.addEventListener("click", () => {
   const greetingBlock = document.querySelector(".greeting");
   greetingBlock.classList.add("hidden");
+
   const gameboardBlock = document.querySelector(".gameboard");
   gameboardBlock.append(generateButton().chooseOpponent("player", "bot"));
 });
+
+const againstButtons = document.querySelectorAll(".btn-choice");
+againstButtons.addEventListener("click", () => {});
+
+// I need setup state, move your ships to desired locations
 
 // const gameboardBlock = document.querySelector(".gameboard");
 // gameboardBlock.append(gameboardRender().generateTable("Player 1"));
